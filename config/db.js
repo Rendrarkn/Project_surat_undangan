2eeb7a3b5a4e44dbd3528a79403ea3fa4a4582c1
nikel1208 <astrihabibi88@gmail.com>
2025-09-23 14:09:46 +0700

    merge

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
-  database:'latihan_db_crud'
+  password: '' || 'root',
+  database: 'surat_undangan_db'
 });
 
 connection.connect(error => {
