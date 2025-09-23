320b6d04d1bbb44be14a07ed753cccda707f8549
Rendrarkn <yogaandika0504@gmail.com>
2025-09-23 10:47:25 +0700

    pull ges

--- routes/invitations.js
+++ routes/invitations.js
@@ -12,56 +12,35 @@ router.get('/', (req, res) => {
     });
 });
 
-// Show add form
-router.get('/add', (req, res) => {
-    res.render('add');
-});
 
-// Add new invitation
-router.post('/add', (req, res) => {
-    const newInvitation = { 
-        nama_undangan: req.body.nama_undangan, 
-        tanggal_acara: req.body.tanggal_acara,
-        lokasi: req.body.lokasi,
-        deskripsi: req.body.deskripsi
-    };
-    const sql = "INSERT INTO surat_undangan SET ?";
-    db.query(sql, newInvitation, (err, result) => {
-        if (err) throw err;
-        res.redirect('/');
-    });
-});
+const ExcelJS = require('exceljs');
 
-// Show edit form
-router.get('/edit/:id', (req, res) => {
-    const sql = `SELECT * FROM surat_undangan WHERE id = ${req.params.id}`;
-    db.query(sql, (err, result) => {
+// Export to Excel
+router.get('/export', (req, res) => {
+    const sql = "SELECT * FROM surat_undangan";
+    db.query(sql, (err, results) => {
         if (err) throw err;
-        res.render('edit', { invitation: result[0] });
-    });
-});
 
-// Update invitation
-router.post('/edit/:id', (req, res) => {
-    const updatedInvitation = {
-        nama_undangan: req.body.nama_undangan,
-        tanggal_acara: req.body.tanggal_acara,
-        lokasi: req.body.lokasi,
-        deskripsi: req.body.deskripsi
-    };
-    const sql = `UPDATE surat_undangan SET ? WHERE id = ${req.params.id}`;
-    db.query(sql, updatedInvitation, (err, result) => {
-        if (err) throw err;
-        res.redirect('/');
-    });
-});
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet('Undangan');
 
-// Delete invitation
-router.get('/delete/:id', (req, res) => {
-    const sql = `DELETE FROM surat_undangan WHERE id = ${req.params.id}`;
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        res.redirect('/');
+        worksheet.columns = [
+            { header: 'Nama Undangan', key: 'nama_undangan', width: 30 },
+            { header: 'Tanggal Acara', key: 'tanggal_acara', width: 20 },
+            { header: 'Lokasi', key: 'lokasi', width: 30 },
+            { header: 'Deskripsi', key: 'deskripsi', width: 50 }
+        ];
+
+        results.forEach(invitation => {
+            worksheet.addRow(invitation);
+        });
+
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        res.setHeader('Content-Disposition', 'attachment; filename="undangan.xlsx"');
+
+        workbook.xlsx.write(res).then(() => {
+            res.end();
+        });
     });
 });
 
